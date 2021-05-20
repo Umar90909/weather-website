f7de2201cac6dbdf3efda858f3fb1864b87d8d2c
unknown <umeraman92@gmail.com>
2021-05-20 20:49:38 +0500

    forecast

--- src/utilss/forecast.js
+++ src/utilss/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (address, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=8d0d306fc72a8a6b3644690c92635ab3&query=' + address + '&units=m'// + latitude + ',' + longitude + '&units=m'
 
@@ -10,7 +12,8 @@ const forecast = (address, callback) => {
             callback('Unable to find location! weather-app', undefined)
         } else {
             console.log('It is ' + response.body.current.weather_descriptions + ' in ' + response.body.location.name + ', ' + response.body.location.region + '. It is currently ' + response.body.current.temperature + ' degrees out. And It feelslike ' + response.body.current.feelslike + ' And Humidity is ' + response.body.current.humidity)
-            callback(undefined, 'It is ' + response.body.current.weather_descriptions + ' in ' + response.body.location.name + ', ' + response.body.location.region + '. It is currently ' + response.body.current.temperature + ' degrees out. And It feelslike ' + response.body.current.feelslike + ' And Humidity is ' + response.body.current.humidity)
+            console.log(response.body.current)
+            callback(undefined, 'It is ' + response.body.current.weather_descriptions + ' in ' + response.body.location.name + ', ' + response.body.location.region + '.\nIt is currently ' + response.body.current.temperature + ' degrees out. And It feelslike ' + response.body.current.feelslike + ' And Humidity is ' + response.body.current.humidity + ' And Wind Speed is ' + response.body.current.wind_speed + ' And let us know that is it a day? ' + response.body.current.is_day)
         }
     })
 }
