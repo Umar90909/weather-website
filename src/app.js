1e54cb7fab1b47d3058dc7c15d40ab97ec3428e8
unknown <umeraman92@gmail.com>
2021-05-20 15:22:31 +0500

    Init commit

---
+++ src/app.js
@@ -0,0 +1,133 @@
+const path = require('path')
+const express = require('express')
+//const forecast =require('/forecast')
+const hbs = require('hbs')
+const geocode = require('./utilss/geocode')
+const forecast = require('./utilss/forecast')
+
+const app = express()
+
+//testing git by changing
+//Define paths for Express config
+const publicFolderPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//console.log(path.join(__dirname, '../public/'))
+
+//setup handerbars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+//Setup static directory to serve.
+app.use(express.static(publicFolderPath))
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App!',
+        name: 'M. Umar Aman'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me!',
+        name: 'M.Umar Aman'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text!',
+        title: 'Help!',
+        name: 'M. Umar Aman.'
+    })
+
+})
+
+// app.get('', (req, res) => {
+//     res.send('<h1>Weather</h1>')
+// })
+
+// app.get('/help', (req, res) => {
+//     res.send([{
+//         name: 'Andrew',
+//         age: 27
+//     }, {
+//         name: 'Umar'
+//     }])
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<h1>Welcome to Sufi Cloth House!</h1>')
+// })
+
+app.get('/view-weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an Address!'
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(location, (error, forecastData) => {
+
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+    // const obj = [{
+    //     forecast: 'Rainy',
+    //     location: 'Faisalabad!',
+    //     address: req.query.address
+    // }]
+    // res.send(obj)
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide search term!'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+
+// app.com
+//app.com/help
+//app.com/about
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: ' mvsmkdfmsd',
+        errorMessage: 'Page not Found!'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'M. Umkasn Aman',
+        errorMessage: 'Page Not Found!'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000')
+})
---
+++ src/utilss/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (address, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=8d0d306fc72a8a6b3644690c92635ab3&query=' + address + '&units=m'// + latitude + ',' + longitude + '&units=m'
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to Weather Service!', undefined)
+        } else if (response.body.error) {
+            callback('Unable to find location! weather-app', undefined)
+        } else {
+            console.log('It is ' + response.body.current.weather_descriptions + ' in ' + response.body.location.name + ', ' + response.body.location.region + '. It is currently ' + response.body.current.temperature + ' degrees out. And It feelslike ' + response.body.current.feelslike + ' And Humidity is ' + response.body.current.humidity)
+            callback(undefined, 'It is ' + response.body.current.weather_descriptions + ' in ' + response.body.location.name + ', ' + response.body.location.region + '. It is currently ' + response.body.current.temperature + ' degrees out. And It feelslike ' + response.body.current.feelslike + ' And Humidity is ' + response.body.current.humidity)
+        }
+    })
+}
+
+module.exports = forecast
